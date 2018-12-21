742a8866197762c5157aa7a8b2d69f73e9162416
Dennis Spicker <dennisspicker@gmx.de>
2018-12-22 00:30:30 +0100

    Add a Print function to node class.

--- class_node.cpp
+++ class_node.cpp
@@ -1,6 +1,7 @@
 #include "class_node.h"
 
-
+#include <iostream>
+#include <array>
 
 pfNode::pfNode(int t){
   // TODO: catch wrong values of t .
@@ -33,4 +34,22 @@ std::string pfNode::GetTypeName(){
       return "Undefined" ;
   }
 
+}
+
+void pfNode::Print(){
+  std::array<int,3> color = {0,0,0};
+  switch (type) {
+    case 1 :         // Wall
+      color = {180,180,180};
+      break;
+    case 2 :        // Grass
+      color = {51, 204, 51};
+      break;
+    case 3 :        // Forest
+      color = {0, 153, 51};
+      break;
+  }
+  char buffer1[50];
+  sprintf(buffer1, "\033[48;2;%u;%u;%um  \033[0m", color[0],color[1],color[2]);
+  std::cout << buffer1 ;
 }
--- class_node.h
+++ class_node.h
@@ -19,6 +19,8 @@ public:
 
   std::string GetTypeName() ;
 
+  void Print();
+
 };
 
 
