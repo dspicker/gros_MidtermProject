10e0f1bb984a48873a9f233e27f89093a20cc406
Felix Ahlbrecht <felix.ahlbrecht@gmail.com>
2019-01-07 21:57:49 +0100

    update uniformCost.cpp

--- uniformCost.cpp
+++ uniformCost.cpp
@@ -1,1 +1,18 @@
-#include "class_unifromCost.h"
+#include "class_map.h"
+
+#include <iostream>
+#include <vector>
+#include <priority_queue>
+#include <queue>   // probably not needed..
+
+// declaration of helper functions
+
+
+//algorithm
+pfMap uniformCost(pfMap map){
+  //or just pass Object of class pfMap to algorithm?
+  return map;
+}
+
+
+// definition of helper functions
