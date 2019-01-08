c4d87d2c5d4e976753f7ed76ad0dadc3234dd1f0
Felix Ahlbrecht <felix.ahlbrecht@gmail.com>
2019-01-08 18:22:40 +0100

    some generalisation

--- experimentation/priority_queue.cpp
+++ experimentation/priority_queue.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <array>
+
+//TODO: https://stackoverflow.com/questions/649640/how-to-do-an-efficient-priority-update-in-stl-priority-queue
 
 // function to print contnent of priority_queue from last to first (?)
 template<typename T>
@@ -11,23 +14,41 @@ void printQ(T& q){  // can only be used once
   }
   std::cout << std::endl;
 }
+// compare function (only integer)
 bool compare(int a, int b){
   return a>b;
 }
+// function to fill que
+template<typename T>
+void fillQ(T &q, int arr[]){
+  for(int i=0;i<3;i++){
+    q.push(arr[i]);
+  }
+}
+
 
 // main
 int main(){
-  int intArr[] = {1,5,9,10,30,3,3,4,1};
+  int a = 1;
+  int b = 4;
+  int c = 2;
+  int intArr[] = {a,b,c};
 
   // default
   std::priority_queue<int> q;
-  for(int i : intArr)
-    q.push(i);
+//  for(int i : intArr)
+//    q.push(i);
+  fillQ(q, intArr);
   printQ(q);
 
   // with custom compare function using lambda:
-  // (alternative would be structure with comparison function..?)
   auto comp = [](int a, int b){return a>b;};
   std::priority_queue<int,std::vector<int>, decltype(comp)> q2(comp);
+  fillQ(q2, intArr);
+  printQ(q2);
+
+  // using normal function compare
   std::priority_queue<int, std::vector<int>, decltype(&compare)> q3(&compare);
+  fillQ(q3, intArr);
+  printQ(q3);
 }
