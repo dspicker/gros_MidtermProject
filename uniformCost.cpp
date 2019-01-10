a9e1f2a48fb31841555dd6cb57a2f7a466b0c90a
Felix Ahlbrecht <felix.ahlbrecht@gmail.com>
2019-01-10 21:07:33 +0100

    updated comments in code

--- uniformCost.cpp
+++ uniformCost.cpp
@@ -9,36 +9,29 @@
 
 typedef std::array<int,2> locArr;
 
-bool debug = false;  // display test output at different stages of algorithm
+bool debug = true;  // display test output at different stages of algorithm
 
-/*
-For this algorithm pfNode need an additional integer varaiable "cumulative cost"
-containing the distance to the starting node.
-
-It also would be helpful if there was a possibility to mark a node as visited,
-for example with an additional boolean variable "visited".
-Alternatively it would be possible to keep track of visited nodes by storing them
-in a container and searching for them at every step (bad performance!)
-
-...
+/* some thoughts regarding the class pfNodes:
+A variable 'int cumCost' of pfNode would be usefull to boost performance of
+uniformCost by reducing usage of slow std::maps to store cumCost
+AND
+to make use of a single comparison between already assigned cumCost and
+newly calculated cumCost (by starting with cumCost=INT_MAX and just checking if
+calculated is smaller than assigned).
 */
 
 /* TODO
-    - overlad operator+ for type locArr
+    - draw path onto map!
+    - add functionality to messure time taken to find the targetLoc
     - add function to pfMap class to set start and target locations ALSO
       in the varialbes start_loc and target_loc of pfMap
       (otherwise it is impossible to set them..)
-    - reconsider where exactly the cumCost is taken into accoutn
-      is the weight of currentLoc part of its cumCost ???
-      (the way it is implemented now: yes, but I'm not sure if this is the
-      best decision anymore, it might not make a difference..?)
-
-      .. yeah it should not make a difffernece. It's eighther starting width
-      cumCost=0 of at the startLoc and taking the weight of the target node
-      into account
-      OR
-      it's the other way arround, starting with cumCost=1, which is not very
-      sensical..
+    - test with small random maps! (especially case with no path)
+    - optimize time by minimizing use of maps history and cumCostMap
+      in loop through neighbors
+    - use 2D array 'cumCostArr' instead of cumCostMap for better performance?
+      (using library arrays, initializing every location with INT_MAX)
+    - introduce boolean varialbe targetFound to handle impossible case ?
 */
 
 // helper functions
@@ -52,12 +45,12 @@ void setHistory(std::map<locArr, locArr> &myMap, locArr current, locArr previous
   myMap[current] = previous;
 }
 void setCumCost(std::map<locArr, int> &myMap, locArr loc, int cost){
+  // change to only set cumCost, if there is no (lower) value saved already??
   myMap[loc] = cost;
 }
 int  getCumCost(std::map<locArr, int> &myMap, locArr loc){
   return myMap.at(loc);
 }
-// only change set-/getCumCost if cumCost can be stored in node directly
 locArr addLocArr(locArr &a, locArr &b){
   locArr res;
   for(int i=0; i<2;i++)
@@ -68,32 +61,30 @@ locArr addLocArr(locArr &a, locArr &b){
 
 //algorithm
 pfMap* uniformCost(pfMap &map){
-  std::map<locArr, int> cumCostMap;
-  std::map<locArr, locArr> history;   // link visited locations to predecessor
-                                      // also used to check if already
 
-  // using lamda expression for custom compare function, pass cumCostMap from uniformCost scope
-  // for more information see: https://en.cppreference.com/w/cpp/language/lambda
+  // initializing containers
+  std::map<locArr, int> cumCostMap;
+  std::map<locArr, locArr> history;   // link visited location to predecessor
+  // using lamda expression: https://en.cppreference.com/w/cpp/language/lambda
   auto cheapest = [&cumCostMap](locArr a, locArr b){
-    // change < to > (or Vice Versa, I dont know yet)
-    // to change from breadth first to depth first!
+    /* on unweighted map:
+    using '>' : similar to breadthfirst algorithm (slow, optimal path)
+    using '<' : similar to depthfirst algorithm (fast, suboptimal path)
+    */
     return (cumCostMap.at(a) > cumCostMap.at(b));
   };
   std::priority_queue<locArr, std::vector<locArr>, decltype(cheapest)> unvisitedPQ(cheapest);
-  // ^ PQ to keep track of unvisited nodes
+  std::array<locArr,4> directions {{{0,1},{1,0},{0,-1},{-1,0}}}; // to loop through neigbors
 
-  // initialize needed variables (and maybe not so needed variables)
+  // initializing varialbes
   locArr startLoc = map.GetStartLoc();
   locArr targetLoc = map.GetTargetLoc();
   locArr currentLoc;
   locArr neighborLoc;
   locArr previousLoc;
-
   int neighborCumCost;
   int currentCumCost;
-
-  std::array<locArr,4> directions {{{0,1},{1,0},{0,-1},{-1,0}}};
-
+  bool targetFound = false;
 
   // set cumCost for start node, push into PQ
   setCumCost(cumCostMap, startLoc, 0);
@@ -107,38 +98,31 @@ pfMap* uniformCost(pfMap &map){
     unvisitedPQ.pop();
     if(wasVisited(history,currentLoc))  // abourt iteration if already visited
       continue;
-
     history[currentLoc] = previousLoc;
     previousLoc = currentLoc;
-
     if (currentLoc == targetLoc){        // end loop if target is found
       if(debug){std::cout << "DEBUG: target reached!" << '\n';}
+      targetFound = true;
       break;
     }
-
     // check neigbors of current node
     if(debug){std::cout << "DEBUG: current: " << currentLoc[0] << "," << currentLoc[1] << '\n';}
     for(auto direction : directions){
       neighborLoc = addLocArr(currentLoc, direction);
       if(isWall(map, neighborLoc) | wasVisited(history, neighborLoc))  // reduncandy
-        continue;
         /* reduncandy!:
-             remove reduncandy becuaes this is also checked at start of while-loop
-             maybe try to check value and handly error if key invalid?
+           remove reduncandy because this is also checked at start of while-loop
+           maybe try to check value and handly error if key invalid?
 
-             An alternavive would be to not check wasVisited at all here
-             because cumCost wouldn't be updated then anyways..
+           An alternavive would be to not check wasVisited at all here
+           because cumCost wouldn't be updated then anyways..
         */
-
-
-
-      /* IMPORTANT: make sure NOT to overwrite a smaller cumCost already entered in cumCostMap!!!
-      // Also there is some out of range problem ..
-      */
+        continue;
+      /* IMPORTANT: make sure NOT to overwrite a smaller cumCost already entered in cumCostMap!!!*/
       currentCumCost = getCumCost(cumCostMap, currentLoc);
       neighborCumCost = currentCumCost + map.GetNodeAt(neighborLoc)->GetWeight();
 
-      // This is not right ATM
+      // This is not optimal ATM (works though..)
       if( !cumCostMap.count(neighborLoc)){
         setCumCost(cumCostMap, neighborLoc, neighborCumCost); // <- does not work as intended!
         unvisitedPQ.push(neighborLoc);
@@ -150,6 +134,10 @@ pfMap* uniformCost(pfMap &map){
       if(debug){std::cout << "DEBUG: neighbor:  " << neighborLoc[0] << "," << neighborLoc[1] << "\n";}
     }
   }
+  if(!targetFound){
+    if(debug){std::cout << "DEBUG: target not reachable!: " << '\n';}
+  }
+
   if(debug){std::cout << "DEBUG: cumCost of target: " << getCumCost(cumCostMap, targetLoc) << '\n';}
   map.PrintMap();
   return &map;
