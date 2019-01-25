a1d2817687ab6fbc85935d801a3aa90844f9df54
Felix Ahlbrecht <felix.ahlbrecht@gmail.com>
2019-01-25 03:04:16 +0100

    add option to animate

--- visualize.cpp
+++ visualize.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "class_node.h"
 #include "class_map.h"
@@ -7,7 +8,7 @@
 #include "class_AStar.h"
 
 int main(){
-  pfMap compare1(*pfMap::LoadMap("maps/map1.csv"));
+  pfMap compare1(*pfMap::LoadMap("maps/compare1.csv"));
 
   // copy maps for all algorithms
   pfMap compare1BF(compare1);
@@ -17,40 +18,50 @@ int main(){
   pfMap compare1ASMin(compare1);
   pfMap compare1ASSup(compare1);
 
-  std::cout << "breadthfirst:" << '\n';
-  Breadthfirst(compare1BF,1);
-  compare1BF.PrintMap();
+  bool animate = false;
+  bool visualize = false;
 
-  std::cout << "uniform cost" << '\n';
-  uniformCost(compare1UC,1);
+
+  std::cout << "animate?" << '\n';
+  std::cout << "(yes/no):";
+  std::string input;
+  std::cin >> input;
+  if(input == "yes")
+    animate = true;
+  else if(input=="no")
+    visualize = true;
+  else
+    return 1;
+
+
+  std::cout << "breadthfirst:" << '\n';
+  Breadthfirst(compare1BF,visualize, animate);
+  if(!animate){compare1BF.PrintMap();}
+
+  std::cout << "uniform cost:" << '\n';
+  uniformCost(compare1UC,visualize, animate);
   compare1UC.PrintMap();
 
-  std::cout << "A* (manhattan)";
+  std::cout << "A* (manhattan):";
   pfAStar ASMan(compare1ASMan);
-  ASMan.solve("Manhattan",1);
-  compare1ASMan.PrintMap();
+  ASMan.solve("Manhattan",visualize, animate);
+  if(!animate){compare1ASMan.PrintMap();}
 
-
-  std::cout << "A* (euklid)";
+  std::cout << "A* (euklid):";
   pfAStar ASEuk(compare1ASEuk);
-  ASEuk.solve("Euklid",1);
-  compare1ASEuk.PrintMap();
+  ASEuk.solve("Euklid",visualize, animate);
+  if(!animate){compare1ASEuk.PrintMap();}
 
-
-  std::cout << "A* (minimum)";
+  std::cout << "A* (minimum):";
   pfAStar ASMin(compare1ASMin);
-  ASMin.solve("Minimum",1);
-  compare1ASMin.PrintMap();
+  ASMin.solve("Minimum",visualize, animate);
+  if(!animate){compare1ASMin.PrintMap();}
 
-
-  std::cout << "A* (supremum)";
+  std::cout << "A* (supremum):";
   pfAStar ASSup(compare1ASSup);
-  ASSup.solve("Supremum",1);
-  compare1ASSup.PrintMap();
+  ASSup.solve("Supremum",visualize, animate);
+  if(!animate){compare1ASSup.PrintMap();}
 
 
-
-
-
-
+  return 0;
 }
